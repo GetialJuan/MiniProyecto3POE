fff3ee3c841d1bf323a8411f4e313c838fd87926
tu <cambio@ejemplo.com>
2022-07-14 16:50:15 -0500

    Se desarrollo la clase Contacto

---
+++ src/modelo/Contacto.java
@@ -0,0 +1,53 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package modelo;
+
+import java.util.HashMap;
+import java.util.Map;
+import java.util.Vector;
+
+/**
+ *
+ * @author invitado
+ */
+public class Contacto {
+    
+    private String fechaDeNacimiento;
+    private String iD;
+    private String nombres;
+    private String apellidos;
+    private String estamento;
+    private Vector<Map<String, String>> direcciones;
+    private Vector<Map<String, String>> telefonos;
+    
+    public Contacto(String fechaDeNacimiento, String iD, 
+            String nombres, String apellidos, String estamento) {
+        this.fechaDeNacimiento = fechaDeNacimiento;
+        this.iD = iD;
+        this.nombres = nombres;
+        this.apellidos = apellidos;
+        this.estamento = estamento;
+        
+        direcciones = new Vector<>();
+        telefonos = new Vector<>();
+    }
+    
+    public void agregarDireccion(String direccion,String barrio,String ciudad){
+        Map<String,String> dir = new HashMap();
+        dir.put("direccion", direccion);
+        dir.put("barrio", barrio);
+        dir.put("ciudad", ciudad);
+        
+        direcciones.add(dir);
+    }
+    
+    public void agregarTelefono(String numero, String tipo){
+        Map<String,String> telefono = new HashMap();
+        telefono.put("numero", numero);
+        telefono.put("tipo", tipo);
+        
+        telefonos.add(telefono);
+    }
+}

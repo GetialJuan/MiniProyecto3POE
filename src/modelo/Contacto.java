5e4fbc67b52c34166df966f5cd31f336f6113b4f
GetialJuan <getial.juan@correounivalle.edu.co>
2022-07-15 20:35:39 -0500

    Se agrego la funcion de agregar contacto al txt

--- src/modelo/Contacto.java
+++ src/modelo/Contacto.java
@@ -22,16 +22,17 @@ public class Contacto {
     private Vector<Map<String, String>> direcciones;
     private Vector<Map<String, String>> telefonos;
     
-    public Contacto(String fechaDeNacimiento, String iD, 
-            String nombres, String apellidos, String estamento) {
+    public Contacto(String fechaDeNacimiento, String iD, String nombres, 
+            String apellidos, String estamento, 
+            Vector<Map<String, String>> direcciones, 
+            Vector<Map<String, String>> telefonos) {
         this.fechaDeNacimiento = fechaDeNacimiento;
         this.iD = iD;
         this.nombres = nombres;
         this.apellidos = apellidos;
         this.estamento = estamento;
-        
-        direcciones = new Vector<>();
-        telefonos = new Vector<>();
+        this.direcciones = direcciones;
+        this.telefonos = telefonos;
     }
     
     public void agregarDireccion(String direccion,String barrio,String ciudad){
@@ -42,12 +43,13 @@ public class Contacto {
         
         direcciones.add(dir);
     }
-    
+    //estos metodos pueden ser utiles en el futuro
+    /*
     public void agregarTelefono(String numero, String tipo){
         Map<String,String> telefono = new HashMap();
         telefono.put("numero", numero);
         telefono.put("tipo", tipo);
         
         telefonos.add(telefono);
-    }
+    }*/
 }
--- src/modelo/Directorio.java
+++ src/modelo/Directorio.java
@@ -4,6 +4,12 @@
  */
 package modelo;
 
+import java.io.File;
+import java.io.FileWriter;
+import java.io.IOException;
+import java.io.PrintWriter;
+import java.util.ArrayList;
+import java.util.Map;
 import java.util.Vector;
 
 /**
@@ -12,8 +18,38 @@ import java.util.Vector;
  */
 public class Directorio {
     private Vector<Contacto> directorio;
+    private PrintWriter pwDirectorio;
 
-    public Directorio() {
+    public Directorio(File txtDirectorio) {
         directorio = new Vector();
+        try {
+            pwDirectorio = new PrintWriter(new FileWriter(txtDirectorio,true));
+        } catch (IOException ex) {
+            System.out.println(ex.getMessage());
+        }
+        //Se ecribe la primera linea en el archivo
+        pwDirectorio.println("id,nombres,apellidos,fechaDeNacimiento,estamento,"
+                + "telefonos,direcciones");
+        
     }
+    
+    public void agregarContacto(String fechaDeNacimiento, String iD, 
+            String nombres, String apellidos, String estamento, 
+            Vector<Map<String,String>> direcciones, 
+            Vector<Map<String,String>> telefonos)
+    {
+        
+        Contacto nuevoContacto = new Contacto(fechaDeNacimiento, iD, nombres, 
+                apellidos, estamento, direcciones, telefonos);
+        
+        directorio.add(nuevoContacto);
+        
+        pwDirectorio.print(iD+","+nombres+","+apellidos+","+fechaDeNacimiento
+                +","+estamento+",");
+        for(Map<String,String> e : telefonos){
+            pwDirectorio.print(e);
+        }
+    }
+    
+    
 }
